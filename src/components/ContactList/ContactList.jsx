06b90c6589b5ba42081eb1f8cb12f83e5e04d9d3
Dima <dima@vantage.software>
2024-10-13 20:40:38 +0300

    finished hw6

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -2,18 +2,24 @@ import styles from "./contactList.module.css";
 import Contact from "../Contact/Contact.jsx";
 import { useDispatch, useSelector } from "react-redux";
 import { deleteContact, selectContacts } from "../../redux/contactsSlice.js";
+import { selectNameFilter } from "../../redux/filtersSlice.js";
 
 const ContactList = () => {
   const contacts = useSelector(selectContacts);
+  const search = useSelector(selectNameFilter);
   const dispatch = useDispatch();
 
   const onDelete = (id) => {
     dispatch(deleteContact(id));
   };
 
+  const filteredContacts = contacts.filter((contact) => {
+    return contact.name.toLowerCase().includes(search.toLowerCase());
+  });
+
   return (
     <div className={styles.contacts}>
-      {contacts.map((contact) => (
+      {filteredContacts.map((contact) => (
         <Contact
           key={contact.id}
           name={contact.name}
